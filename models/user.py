d57ac64783cf1ba3344fc987b05ac4471c2a1f4d
Eric <looicen66@gmail.com>
2020-10-19 15:14:07 +0800

    user sign-up

--- models/user.py
+++ models/user.py
@@ -1,6 +1,19 @@
 from models.base_model import BaseModel
 import peewee as pw
+from flask import flash
 
 
 class User(BaseModel):
-    name = pw.CharField(unique=False)
+    username = pw.CharField(unique=True, null=False)
+    email = pw.CharField(unique=True, null=False)
+    password = pw.CharField(null=False)
+
+    def validate(self):
+        duplicate_username = User.get_or_none(User.username == self.username)
+
+        if duplicate_username:
+            self.errors.append(
+                'Username is already taken. Please choose something else.')
+
+        else:
+            flash("Username available!")
