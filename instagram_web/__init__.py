6034d59a8cad5b2bab2f1707eb94da330812e4e6
Eric <looicen66@gmail.com>
2020-10-19 17:33:56 +0800

    WIP: custom error messages, missing links

--- instagram_web/__init__.py
+++ instagram_web/__init__.py
@@ -15,6 +15,10 @@ app.register_blueprint(sessions_blueprint, url_prefix="/sessions")
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 @app.route("/")
 def home():
